9603d0f78c9c859e0936804048aa648c004bf6bf
Chuks1993 <chuksgrinage@gmail.com>
2017-12-14 14:52:58 -0600

    hey

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,17 @@
 $(document).ready(function(){
 
 
-
-
-    var userPick;
-    var answers = {
+$.fn.trivia = function() {
+    var _t = this;
+    _t.userPick = null;
+    _t.answers = {
         correct: 0,
         incorrect: 0
     };
-    var images;
-    var count = 5;
-    var myVar;
-    
-    var current = 0;
-    var questions = [{
+    _t.images = null;
+    _t.count = 30;
+    _t.current = 0;
+    _t.questions = [{
         question: "What is 50 Cent's real name?",
         choices: ["Curtis Jackson", "Marshal Mathers", "Jimmy Newtron"],
         images: ["../images/50-cent.gif"],
@@ -29,100 +27,92 @@ $(document).ready(function(){
         choices: ["Sony Records", "RocaFella", "Good Music"],
         images: ["../images/kanye-west.gif"],
         correct: 2
+
     }];
+    _t.ask = function() {
+        if (_t.questions[_t.current]) {
+            $("#timer").html( _t.count);
+            $("#question_div").html(_t.questions[_t.current].question);
+            var choicesArr = _t.questions[_t.current].choices;
+            var buttonsArr = [];
 
-   
-
-   
-   $('#start').click(function () {
-       timer();
-       trivia();
-       
-   })
-
-    //timer 
-
-   function timer() {
-       myVar = setInterval(countDown, 1000);
-   }
-
-   function countDown () {
-        count--;
-        $('#timer').html(count)
-        
-
-        if (count === 0) {
-            count = 5;
-            current++;
-            console.log(current);
-            $('#choices_div').empty();
-            trivia();
-        }
-   }
-
-   //display questions
-
-   function trivia() {
-       $("#question_div").html(questions[current].question);
-       var choicesArr = questions[current].choices;
-       var buttonsArr = [];
-   
-
-   for (var i = 0; i < choicesArr.length; i++) {
-                var button = $('<button>');
+            for (var i = 0; i < choicesArr.length; i++) {
+                var button = $('<button class="btn btn-warning">');
                 button.text(choicesArr[i]);
                 button.attr('data-id', i);
                 $('#choices_div').append(button);
             }
-}
-     $('#choices_div').on('click', 'button', function(e) {
-       userPick = $(this).data("id"),
-        index = questions[current].correct;
-        correct = questions[current].choices[index];
+            window.triviaCounter = setInterval(_t.timer, 1000);
+        } else {
+            $('body').append($('<div />', {
+                text: 'Unanswered: ' + (
+                    _t.questions.length - (_t.answers.correct + _t.answers.incorrect)),
+                class: 'result'
+            }));
+            $('#start_button').text('Restart').appendTo('body').show();
+        }
+    };
+    _t.timer = function() {
+        _t.count--;
+        if (_t.count <= 0) {
+            setTimeout(function() {
+                _t.nextQ();
+            });
+
+        } else {
+            $("#timer").html( _t.count );
+        }
+    };
+    _t.nextQ = function() {
+        _t.current++;
+        clearInterval(window.triviaCounter);
+        _t.count = 30;
+        $('#timer').html("");
+        setTimeout(function() {
+            _t.cleanUp();
+            _t.ask();
+        }, 1000)
+    };
+    _t.cleanUp = function() {
+        $('div[id]').each(function(item) {
+            $(this).html('');
+        });
+        $('.correct').html('Correct answers: ' + _t.answers.correct);
+        $('.incorrect').html('Incorrect answers: ' + _t.answers.incorrect);
+    };
+    _t.answer = function(correct) {
+        var string = correct ? 'correct' : 'incorrect';
+        _t.answers[string]++;
+        $('.' + string).html(string + ' answers: ' + _t.answers[string]);
+    };
+    return _t;
+};
+var Trivia;
+
+$("#start_button").click(function() {
+    $(this).hide();
+    $('.result').remove();
+    $('div').html('');
+    Trivia = new $(window).trivia();
+    Trivia.ask();
+});
+
+$('#choices_div').on('click', 'button', function(e) {
+    var userPick = $(this).data("id"),
+        _t = Trivia || $(window).trivia(),
+        index = _t.questions[_t.current].correct,
+        correct = _t.questions[_t.current].choices[index];
 
     if (userPick !== index) {
         $('#choices_div').text("Wrong Answer! The correct answer was: " + correct);
-        answers.incorrect++;
+        _t.answer(false);
     } else {
         $('#choices_div').text("Correct!!! The correct answer was: " + correct);
-        answers.corret++;
+        _t.answer(true);
     }
-    
+    _t.nextQ();
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
 
 });
